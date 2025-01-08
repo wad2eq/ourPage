e37988bbd3d69d55e3fd566c4936fbfee473c499
wad2eq <mirekkedzierski@gmail.com>
2025-01-08 07:01:54 +0100

    Fix SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+import robots from 'astro-robots';
+
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), robots()]
 });
